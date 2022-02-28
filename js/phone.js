1e8978551ba5b3d10e234c27051196bd5469aac8
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-02-28 21:38:06 +0600

    card created

--- js/phone.js
+++ js/phone.js
@@ -6,5 +6,8 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+const displaySearchResult = phones => {
+    console.log(phones);
 }
