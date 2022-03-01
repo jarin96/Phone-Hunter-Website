b0b944091986902830094d96eed7f204e8c3a09a
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-03-01 18:31:49 +0600

    done

--- js/phone.js
+++ js/phone.js
@@ -8,7 +8,7 @@ const searchPhone = () => {
     const searchText = searchField.value;
     // display Spinner
     toggleSpinner('block');
-    console.log(searchText);
+    // console.log(searchText);
     // clear data
     searchField.value = '';
     document.getElementById('error-message').style.display = 'none';
@@ -31,6 +31,7 @@ const searchPhone = () => {
 const displayError = error => {
     document.getElementById('error-message').style.display = 'block';
 }
+// Phone search area
 const displaySearchResult = phones => {
     const selectedPhone = phones.slice(0, 20);
     // console.log(selectedPhone);
@@ -41,7 +42,7 @@ const displaySearchResult = phones => {
         document.getElementById('error').style.display = 'block';
     }
     selectedPhone?.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -61,14 +62,15 @@ const displaySearchResult = phones => {
     toggleSpinner('none');
 }
 const loadPhoneDetail = phoneId => {
-    console.log(phoneId);
+    // console.log(phoneId);
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetail(data.data));
 }
+// single phone detail
 const displayPhoneDetail = detail => {
-    console.log(detail.releaseDate);
+    // console.log(detail.releaseDate);
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = '';
     const div = document.createElement('div');
@@ -76,21 +78,21 @@ const displayPhoneDetail = detail => {
     div.innerHTML = `
     <img src="${detail.image}" class="card-img-top w-50 mx-auto" alt="...">
     <div class="card-body">
-        <h5 class="card-title">Name: ${detail.name}</h5>
-        <p class="card-text">Release Date: ${detail.releaseDate ? detail.releaseDate : 'No Release Date Found'}</p>
+        <h5 class="card-title">Name : ${detail.name}</h5>
+        <p class="card-text">Release Date : ${detail.releaseDate ? detail.releaseDate : 'No Release Date Found'}</p>
         <p>Brand: ${detail.brand}</p>
-        <h5>Storage: ${detail.mainFeatures.storage}</h5>
-        <h5>DisplaySize: ${detail.mainFeatures.displaySize}</h5>
-        <h5>Chipset: ${detail.mainFeatures.chipSet}</h5>
-        <h5>Memory: ${detail.mainFeatures.memory}</h5>
-        <h5>Sensors: ${detail.mainFeatures.sensors}</h5>
+        <h5><span class="fw-bolder">Storage :</span> ${detail.mainFeatures.storage}</h5>
+        <h5><span class="fw-bolder">DisplaySize :</span> ${detail.mainFeatures.displaySize}</h5>
+        <h5><span class="fw-bolder">Chipset :</span> ${detail.mainFeatures.chipSet}</h5>
+        <h5><span class="fw-bolder">Memory :</span> ${detail.mainFeatures.memory}</h5>
+        <h5><span class="fw-bolder">Sensors :</span> ${detail.mainFeatures.sensors}</h5>
         <h5>Others Information</h5>
-         <h5>Bluetooth: ${detail.others?.Bluetooth ? detail.others.Bluetooth : 'No Bluetooth Found'}</h5>
-         <h5>GPS: ${detail.others?.GPS ? detail.others.GPS : 'No GPS Found'}</h5>
-         <h5>NFC: ${detail.others?.NFC ? detail.others.NFC : 'No NFC Found'}</h5>
-         <h5>Radio: ${detail.others?.Radio ? detail.others.Radio : 'No Radio Found'}</h5>
-         <h5>USB: ${detail.others?.USB ? detail.others.USB : 'No USB Found'}</h5>
-         <h5>WLAN: ${detail.others?.WLAN ? detail.others.WLAN : 'No WLAN Found'}</h5>
+         <h5><span class="fw-bolder">Bluetooth :</span> ${detail.others?.Bluetooth ? detail.others.Bluetooth : 'No Bluetooth Found'}</h5>
+         <h5><span class="fw-bolder">GPS :</span> ${detail.others?.GPS ? detail.others.GPS : 'No GPS Found'}</h5>
+         <h5><span class="fw-bolder">NFC :</span> ${detail.others?.NFC ? detail.others.NFC : 'No NFC Found'}</h5>
+         <h5><span class="fw-bolder">Radio :</span> ${detail.others?.Radio ? detail.others.Radio : 'No Radio Found'}</h5>
+         <h5><span class="fw-bolder">USB :</span> ${detail.others?.USB ? detail.others.USB : 'No USB Found'}</h5>
+         <h5><span class="fw-bolder">WLAN :</span> ${detail.others?.WLAN ? detail.others.WLAN : 'No WLAN Found'}</h5>
     </div>`;
     phoneDetails.appendChild(div);
 }
